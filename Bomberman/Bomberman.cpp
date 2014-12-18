17f22c707caae2b166f3dbb5d005b73cfc34b99e
Emil Elthammar <emilelthammar@gmail.com>
2014-12-18 07:40:05 +0100

    Reseted main function to work with game again

--- Bomberman/Bomberman.cpp
+++ Bomberman/Bomberman.cpp
@@ -8,22 +8,10 @@
 
 int _tmain(int argc, _TCHAR* argv[])
 {
-
-	MapGenerator m(10,5);
-	//m.SaveMap();
-	
-	m.LoadMap();
-	m.Print();
-	std::cin.get();
-
-
-
-	/*
 	Engine engine;
 	if (engine.Initialize())
 		engine.Update();
 	engine.Shutdown();
-	*/
 	return 0;
 }
 
