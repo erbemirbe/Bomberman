f3d87bc90e2595c2bc01a57997b23c44a7342b3d
erbemirbe <emilelthammar@gmail.com>
2015-01-05 16:07:03 +0100

    Bomberman not arkanoid!

--- Bomberman/DrawManager.cpp
+++ Bomberman/DrawManager.cpp
@@ -17,7 +17,7 @@ DrawManager::~DrawManager()
 
 bool DrawManager::Initialize(int width, int height)
 {
-	m_window = SDL_CreateWindow("Arkanoid",
+	m_window = SDL_CreateWindow("Bomberman",
 		SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
 		width, height, SDL_WINDOW_OPENGL);
 	if (m_window == nullptr)
